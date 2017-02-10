fc1604c39e805e094f949f3f787f0ea418746976
ciaranmaher95 <ciaranmaher13@outlook.com>
2017-02-10 13:19:43 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once("includes/database.php");
+?>
+<link href="css/bootsrap.css" rel="stylesheet" type="text/css"/>
+<link href="css/awesomefonts.css" rel="stylesheet" type="text/css"/>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span12">
+            <div class="navbar navbar-fixed-top navbar-inverse">
+                <div class="navbar-inner ">
+                    <div class="container">
+                        <a href="index.php?action=home" class="brand">Language Courses</a>
+                        <ul class="nav pull-left">
+                            <li><a href="index.php?action=home">Home</a></li>
+                            <li><a href="index.php?action=about_us">Products</a></li>
+                            <li class="divider-vertical"></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
